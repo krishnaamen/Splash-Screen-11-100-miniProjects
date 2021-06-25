0f67701917bf9af21672131149c970ec6af428f4
Krishna Prasad Khanal <krishnaamen@gmail.com>
2021-06-25 08:11:45 +0200

    Initial Commit

---
+++ Splash-Screen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Splash-Screen
+//
+//  Created by Macbook on 25/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
